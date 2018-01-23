fdf5e52faef029baa49dcd6862983babcd71dcdd
Daniel Klein <dklein@idmod.org>
2018-01-23 02:40:59 -0800

    Adding progress plotting.

--- history_matching/__init__.py
+++ history_matching/__init__.py
@@ -2,3 +2,5 @@ from .basis import Basis
 from .quick_read import quick_read
 from .HistoryMatching import HistoryMatching
 from .HistoryMatchingCut import HistoryMatchingCut
+from .ProgressPlotting import ProgressPlotting
+from .glm import GLM
