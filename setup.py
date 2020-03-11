9f66f20efbaf3544c0bd804c73493461d84941ec
Richard Barnes <rbarnes@umn.edu>
2020-03-10 23:45:30 -0600

    Use fast-math

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
         'msvc': ['/EHsc'],
-        'unix': [],
+        'unix': ['-ffast-math'],
     }
     l_opts = {
         'msvc': [],
