7eccda317cff041c5d1f7f5725e9d4aad01f5cde
Richard Barnes <rbarnes@umn.edu>
2020-03-27 22:37:53 -0600

    Add scaler

---
+++ hm2/hm2/test/test_utility.py
@@ -0,0 +1,34 @@
+import unittest
+import pandas as pd
+
+from hm2.error import HistoryMatchingError
+from hm2.utility import Scaler
+
+class ScalerTest(unittest.TestCase):
+    def test_not_a_dataframe(self):
+      data = [[1,2,3,4,5],[10,11,12,13,14],[-1,-2,-3,-4,-5]]
+      self.assertRaises(TypeError, Scaler, data)
+
+    def test_columns_mismatch(self):
+      data = pd.DataFrame({"x":[1,2,3,4,5],"y":[10,11,12,13,14],"z":[-1,-2,-3,-4,-5]})
+      s = Scaler(data)
+      data = pd.DataFrame({"m":[1,2,3,4,5],"y":[10,11,12,13,14],"z":[-1,-2,-3,-4,-5]})
+      self.assertRaises(HistoryMatchingError, s.transform, data)
+
+    def test_scales_correctly(self):
+      data = pd.DataFrame({"x":[1,2,3,4,5],"y":[10,11,12,13,14],"z":[-5,-4,-3,-2,-1]})
+      s = Scaler(data)
+      data = pd.DataFrame({"x":[3],"y":[10],"z":[-1]})
+      trans = s.transform(data)
+      self.assertTrue((trans['x']==0.5).all())
+      self.assertTrue((trans['y']==0).all())
+      self.assertTrue((trans['z']==1).all())
+
+    def test_repr(self):
+      data = pd.DataFrame({"x":[1,2,3,4,5],"y":[10,11,12,13,14],"z":[-5,-4,-3,-2,-1]})
+      s = Scaler(data)
+      print(s)
+      self.assertTrue(str(s)=="""  feature   min   max  range
+0       x   1.0   5.0    4.0
+1       y  10.0  14.0    4.0
+2       z  -5.0  -1.0    4.0""")
---
+++ hm2/hm2/utility.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sklearn
+
+from hm2.error import HistoryMatchingError
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+class Scaler:
+  def __init__(self, data):
+    if not isinstance(data, pd.DataFrame):
+      raise TypeError("`data` must be a DataFrame!")
+    self._columns = data.columns.tolist().copy()
+    self._scaler = sklearn.preprocessing.MinMaxScaler().fit(data)
+
+  def transform(self, data):
+    if self._columns!=data.columns.tolist():
+      raise HistoryMatchingError("Columns of DataFrame to be transformed don't match columns Scaler was built on!")
+    return pd.DataFrame(self._scaler.transform(data), columns=self._columns)
+
+  def __repr__(self):
+    return str(pd.DataFrame({
+        "feature": self._columns,
+        "min": self._scaler.data_min_,
+        "max": self._scaler.data_max_,
+        "range": self._scaler.data_range_
+    }))
