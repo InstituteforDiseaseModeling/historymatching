b47b3060dcfbdd64a72e558f02c6b9481e27e71e
Richard Barnes <rbarnes@umn.edu>
2020-03-29 00:36:08 -0600

    Initial import of emulator

---
+++ hm2/hm2/emulator.py
@@ -0,0 +1,58 @@
+import hm2.GLM
+from hm2.gpr import GPR
+
+
+
+class GLM_GPR_Emulator:
+    """Emulator that trains a GLM on data and a GPR on the residuals.
+    """
+    def __init__(
+            self,
+            polyorder, #TODO(r-barnes): Separate glm and gpr polyorder?
+            intercept,
+            family = 'poisson',
+    ):
+        """Initialize the Emulator
+
+        Args:
+            polyorder: Order of polynomial expansion of the data features
+            intercept: Whether to add an intercept feature
+            family: (str) The family of generalized linear model to use. 
+                          Options include 'poisson', 'binomial', 'gamma', 
+                          'negativebinomial', and 'gaussian'. 
+        """
+        self.polyorder = polyorder
+        self.intercept = intercept
+        self.family    = family
+        self.alpha     = None
+        self.model     = None
+
+        self.glm = GLM(polyorder=polyorder, intercept=intercept, faily=family)
+        self.gpr = GPR(polyorder=polyorder, intercept=intercept)
+
+    #TODO(r-barnes): Differentiate glm and gpr maxiter?
+    def fit(self, data, endog, maxiter=1000):
+        """Fit the emulator.
+
+        Args:
+            maxiter: (int)
+                maxiter parameter passed to the statsmodels `fit` function.
+        """
+        self.glm.fit(data, endog, maxiter)
+        residuals = self.glm.residuals(data, endog)
+        self.glm.fit(data, residuals, maxiter)
+
+    def predict(self, data):
+        """Evaluate the emulator and return the mean prediction.
+
+        Args:
+            data: (Pandas DataFrame)
+                Data frame of points similar to training_data.
+
+        Returns:
+            Predicted outputs at the inputs specified by data.
+        """
+        return self.glm.predict(data)+self.glm.predict(data)
+
+    def residuals(self, data, endog):
+        return self.predict(data) - endog
