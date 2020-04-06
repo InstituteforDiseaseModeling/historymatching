7f35bfb7449cdbd88ed08fe22441aa4cec8d2c94
Richard Barnes <rbarnes@umn.edu>
2020-04-06 00:15:50 -0600

    Added plotting.py

---
+++ hm2/hm2/plotting.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+def _plot_data_multi(train_x, train_y, figsize=None, log_scale=False, cmap='viridis', alpha=0.5):
+    """Generates many pair-wise scatter plots of the training data.
+
+    Args:
+        log_scale: Transforms size and color using log(10 * normalized_y_value + 1)
+
+    Returns: a dictionary of matplotlib figure handles with keys indicating
+    the parameter names via the filename which would be used to save the
+    figure.
+    """
+    C = len(train_x.columns)
+
+    individual_plots = dict()
+    collective_fig, collective_ax = plt.subplots(nrows=C, ncols=C)
+
+    for rowi, row in enumerate(train_x):
+        for coli, col in enumerate(train_x):
+            fn = (row,col)
+            x  = train_x[row]
+            y  = train_x[col]
+
+            fig, ax = plt.subplots()
+            ax.scatter(x, y, c=train_y, cmap=cmap, alpha=alpha)
+            ax.set_xlabel(row)
+            ax.set_ylabel(col)
+            fig.tight_layout()
+            individual_plots[fn] = fig
+
+            collective_ax[rowi,coli].scatter(x, y, c=train_y, cmap=cmap, alpha=alpha)
+            collective_ax[rowi,coli].set_xlabel(row)
+            collective_ax[rowi,coli].set_ylabel(col)
+
+    individual_plots['all'] = collective_fig
+
+    return individual_plots
