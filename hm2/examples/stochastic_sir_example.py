338736ad8cfc2586f234883600f60e416262b8aa
Richard Barnes <rbarnes@umn.edu>
2020-04-10 23:39:10 -0600

    Clean up stochastic SIR example

--- hm2/examples/stochastic_sir_example.py
+++ hm2/examples/stochastic_sir_example.py
@@ -109,6 +109,23 @@ matched = match_sim_outputs_to_observations(
 #Fit emulators
 ################################################
 
+# Now we begin the process of emulating the simulation output
+# This process contains two steps.  The first step is to fit a deterministic model, here
+# we use a generalized linear model (GLM).  The glm will attempt to model the output (prevalence 
+# at the first observation) as a function of some inputs.  Those inputs need not be the model
+# parameters directly!  The inputs could be anything from a constant intercept up to third or higher
+# order interaction terms between parameters.  The following Basis instance builds out the GLM input
+# parameters from the overall simulation input parameters.
+#
+# Some strategy is required when choosing these.  If you know which parameters matter, there's a way
+# to directly specify those parameters.  Alternatively, if you have no idea, you can initially include 
+# an intercept, first, second, and maybe also third order interaction terms.  The Basis class has a 
+# built-in penalized regression that throws away unneeded terms (basis vectors).
+# The second step of emulation, as demonstrated here, fits a GPR to the redisual error between the 
+# simulated outputs and the GLM estimates.  If the GLM fits really well, the residual is mostly noise and
+# the GPR has a hard time fitting / isn't very informative.  I actually prefer to weaken the GLM enough
+# to leave plenty of residual signal for the GPR.  Here, I use only first-order (beta and gamma) terms.
+
 psamples_within = hm2.sampling.latin_hypercube_within(parameter_samples, 1000)
 
 #Prepare data for emulator (make a train_x, train_y, stdev_y tuple)
@@ -126,6 +143,11 @@ for obs, params, y, stdev in get_data_for_emulators(parameter_samples, matched[0
 
 implausibilities = get_implausibility(time_emulators, psamples_within, time_observations)
 implausibilities = max_implausibility_per_param(implausibilities)
+
+# The implausibility threshold determines how willing we are to retain regions
+# of parameter space that are inconsistent with the underlying data. A higher
+# threshold is more risk averse in that potentially good regions are less likely
+# to be rejected, however it will take more iterations/simulations to achieve results.
 implausibilities = filter_implausibilities(implausibilities, threshold=0.2)
 plausible_params = get_plausible_parameters(implausibilities, psamples_within)
 
@@ -142,9 +164,17 @@ sys.exit(0)
 
 
 
-#TODO: plot_errors test_mean train_mean
 
-# gpremu.fit(*prepped)
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -158,13 +188,6 @@ sys.exit(0)
 #   hm2.boilerplate.replicate_reducer(runs[1], "mean")
 # )
 
-# a = hm2.boilerplate.extract_training_set_from_keyed_frame(
-#   observation_key = 0,
-#   parameter_samples = parameter_samples,
-#   observations = time_observations,
-#   keyed_frame = mr['time'],
-#   frame_type = 'time',
-# )
 
 
 sys.exit(-1)
@@ -184,191 +207,3 @@ ax[1].scatter(
 )
 
 plt.show()
-
-sys.exit(-1)
-
-
-
-
-################################################
-################################################
-################################################
-################################################
-#Example ends here
-################################################
-################################################
-################################################
-################################################
-
-
-
-
-
-
-# Plot the samples
-# f, ax = plt.subplots(figsize=(6,6));
-# ax.scatter(x=samples[param_info.index.values[0]], y=samples[param_info.index.values[1]]);
-# ax.set_xlim([param_info['Min'][0], param_info['Max'][0]]);
-# ax.set_ylim([param_info['Min'][1], param_info['Max'][1]]);
-# ax.set_xlabel(param_info.index.values[0]);
-# ax.set_ylabel(param_info.index.values[1]);
-
-
-
-################################################
-# Make a cut
-################################################
-
-
-
-# The implausibility threshold determines how willing we are to retain regions
-# of parameter space that are inconsistent with the underlying data. A higher
-# threshold is more risk averse in that potentially good regions are less likely
-# to be rejected, however it will take more iterations/simulations to achieve results.
-# implausibility_threshold = 3
-
-# n_samples_this_iter = 100 # Number of simulations to conduct on this iteration
-# n_samples_to_generate_for_next_iter = 100 # Number of simulations to conduct on this iteration
-
-# training_fraction = 0.75 # Fraction of simulations to use as training
-# discrepancy_std = 3 # Accounts for uncertainty w.r.t model structure
-
-
-# For this first iteration, we're going to make one "cut" using the first observation, but 
-# you can separately do multiple "cuts" per iteration using several observations (separately)
-# We'll need a name for this cut and the desired results
-
-# desired_result_idx = 0 # Pick observation 0 or 1 [integers]
-
-# cut_name = 'Prevalence_Meas_%d'%desired_result_idx # No spaces or strange characters!
-
-# desired_result = observations.iloc[desired_result_idx]['Prevalence']
-# desired_result_var = observations.iloc[desired_result_idx]['Stdev']**2
-
-
-
-
-# Finally we get to do some History Matching!
-
-# Begin by creating an instance of the HistoryMatching class
-# hm = HistoryMatching(
-#     cut_name = cut_name,
-#     param_info = param_info,
-#     inputs = samples,
-#     results = results,
-#     desired_result = desired_result,
-#     desired_result_var = desired_result_var,
-#     iteration = iteration,
-#     implausibility_threshold = implausibility_threshold,
-#     discrepancy_var = discrepancy_std**2,
-#     training_fraction = training_fraction
-# )
-# hm.save() # Save to disk
-
-
-
-code.interact(local=locals())
-
-
-# Now we begin the process of emulating the simulation output
-# This process contains two steps.  The first step is to fit a deterministic model, here
-# we use a generalized linear model (GLM).  The glm will attempt to model the output (prevalence 
-# at the first observation) as a function of some inputs.  Those inputs need not be the model
-# parameters directly!  The inputs could be anything from a constant intercept up to third or higher
-# order interaction terms between parameters.  The following Basis instance builds out the GLM input
-# parameters from the overall simulation input parameters.
-#
-# Some strategy is required when choosing these.  If you know which parameters matter, there's a way
-# to directly specify those parameters.  Alternatively, if you have no idea, you can initially include 
-# an intercept, first, second, and maybe also third order interaction terms.  The Basis class has a 
-# built-in penalized regression that throws away unneeded terms (basis vectors).
-# The second step of emulation, as demonstrated here, fits a GPR to the redisual error between the 
-# simulated outputs and the GLM estimates.  If the GLM fits really well, the residual is mostly noise and
-# the GPR has a hard time fitting / isn't very informative.  I actually prefer to weaken the GLM enough
-# to leave plenty of residual signal for the GPR.  Here, I use only first-order (beta and gamma) terms.
-basis_glm = hm2.basis.PolynomialBasis(
-    degree = 1,
-    intercept = True
-)
-
-
-# Now fit the glm and plot
-
-### GLM ###############################################################
-print("="*80, "\nGeneralized Linear Modeling\n", "="*80)
-#######################################################################
-glm = hm2.glm.GLM(basis=basis_glm, family='Gaussian').fit(runs)
-
-
-
-for file in glob.glob(os.path.join(hm.glmdir, "PairwiseResults", "*.pdf")):
-    img = WImage(filename=file)
-    print(file)
-    display(img)
-
-# Results get saved to disk, so load and display:
-for file in glob.glob(os.path.join(hm.glmdir, "GLM Predicted vs Actual.pdf")):
-    img = WImage(filename=file)
-    print(file)
-    display(img)
-
-code.interact(local=locals())
-sys.exit(-1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Just for fun, let's visualize some SIR trajectories and observations / target data
-# z = SIR(beta=0.003, gamma=0.1)
-# f, ax = z.plot()
-# for i,obs in observations.iterrows():
-#     ax.plot(obs['Times'], obs['Prevalence'], 'ko')
-#     ax.plot(
-#         [obs['Times'],obs['Times']], 
-#         [obs['Prevalence']-2*obs['Stdev'],obs['Prevalence']+2*obs['Stdev']],
-#         'k-')
-
-
-
-
-# Simulation results will ultimately need a 'Sample_Id' and 'Sim_Id'
-# The Sample_Id corresponds to the index in the samples dataframe above
-# You can do more than one replicate of each sample, in which case you'd
-# end up wiht more than one Sim_Id per Sample_Id.  We're not doing that here,
-# so we can basically set Sim_Id to anything.
-sim_results['Sim_Id'] = sim_results['Sample_Id']
-    
-# Plot the observations over the realized trajectories
-for i,obs in observations.iterrows():
-    plt.plot(obs['Times'], obs['Prevalence'], 'ko')
-    plt.plot(
-        [obs['Times'],obs['Times']], 
-        [obs['Prevalence']-2*obs['Stdev'],obs['Prevalence']+2*obs['Stdev']],
-        'k-')
-
-
-
-
-
-
-
-
-
-
-
-# sim_results contains the simulated values at both observation times, but for this iteration
-# we only want to use the first one (ObsIdx == 0).
-# Also note that results must be a Series with index containing 'Sample_Id' and 'Sim_Id'
-results = sim_results \
-    .query('ObsIdx==%d'%desired_result_idx)[['Sample_Id', 'Sim_Id', 'Prevalence']] \
-    .set_index(['Sample_Id', 'Sim_Id'])['Prevalence']
-display(results.tail())
