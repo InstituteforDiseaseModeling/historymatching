a188867581fa76b179e249659e86640c88731455
Richard Barnes <rbarnes@umn.edu>
2020-04-10 22:07:03 -0600

    Simplify emulator interface

--- hm2/hm2/emulators.py
+++ hm2/hm2/emulators.py
@@ -8,18 +8,19 @@ from hm2.plotting import *
 
 
 class EmulatorBase(abc.ABC):
-    def fit(self, train_x, train_y, stdev_y, *args, **kwargs):
+    def _fit(self, train_x, train_y, stdev_y):
         self._train_x = train_x.copy()
         self._train_y = train_y.copy()
         self._stdev_y = stdev_y.copy()
-        return self._fit(train_x, train_y, stdev_y, *args, **kwargs)
+        return train_x, train_y, stdev_y
+
+    def _predict(self, train_x):
+        if "param_id" in train_x.columns:
+            train_x = train_x.drop(columns="param_id")  # TODO
+        return train_x
 
     @abc.abstractmethod
-    def _fit(self):
-        pass
-
-    @abc.abstractmethod
-    def predict(self):
+    def fit(self):
         pass
 
     @abc.abstractmethod
@@ -31,15 +32,14 @@ class EmulatorBase(abc.ABC):
         pass
 
 
-
 class TorchGPREmulator(EmulatorBase):
     def __init__(self, basis):
-        if not isinstance(basis,BasisBase):
+        if not isinstance(basis, BasisBase):
             raise HistoryMatchingError("`basis` must inherit from BasisBase!")
         self.basis = basis
         self.gpr = hm2.gpr.TorchGPR()
 
-    def _fit(self, train_x, train_y, stdev_y, maxiter):
+    def fit(self, train_x, train_y, stdev_y, maxiter):
         """Fit the GPR.
 
         Args:
@@ -50,36 +50,36 @@ class TorchGPREmulator(EmulatorBase):
 
         Returns: None
         """
-        #Extract the relevant parameter sample values
+        train_x, train_y, stdev_y = self._fit(train_x, train_y, stdev_y)
+
+        # Extract the relevant parameter sample values
         self.gpr.fit(
-            train_x = self.basis(train_x),
-            train_y = train_y,
-            stdev_y = stdev_y,
-            maxiter = maxiter
+            train_x=self.basis(train_x),
+            train_y=train_y,
+            stdev_y=stdev_y,
+            maxiter=maxiter,
         )
         return self
 
     def predict(self, test_x):
+        test_x = self._predict(test_x)
         test_x = self.basis(test_x)
         return self.gpr.predict(test_x)
 
     def plot_data(self, *args, **kwargs):
         return plot_data_multi(
-            self.basis(self._train_x),
-            self._train_y,
-            *args, **kwargs
+            self.basis(self._train_x), self._train_y, *args, **kwargs
         )
-
 
 
 class SkGPREmulator(EmulatorBase):
     def __init__(self, basis):
-        if not isinstance(basis,BasisBase):
+        if not isinstance(basis, BasisBase):
             raise HistoryMatchingError("`basis` must inherit from BasisBase!")
         self.basis = basis
         self.gpr = hm2.gpr.SkGPR()
 
-    def _fit(self, train_x, train_y, stdev_y, maxiter):
+    def fit(self, train_x, train_y, stdev_y, maxiter):
         """Fit the GPR.
 
         Args:
@@ -90,40 +90,38 @@ class SkGPREmulator(EmulatorBase):
 
         Returns: None
         """
-        #Extract the relevant parameter sample values
+        train_x, train_y, stdev_y = self._fit(train_x, train_y, stdev_y)
+
+        # Extract the relevant parameter sample values
         self.gpr.fit(
-            train_x = self.basis(train_x),
-            train_y = train_y,
-            stdev_y = stdev_y,
-            maxiter = maxiter
+            train_x=self.basis(train_x),
+            train_y=train_y,
+            stdev_y=stdev_y,
+            maxiter=maxiter,
         )
         return self
 
     def predict(self, test_x):
+        test_x = self._predict(test_x)
         return self.gpr.predict(self.basis(test_x))
 
     def plot_data(self, *args, **kwargs):
         return plot_data_multi(
-            self.basis(self._train_x),
-            self._train_y,
-            *args, **kwargs
+            self.basis(self._train_x), self._train_y, *args, **kwargs
         )
-
 
 
 class GLM_GPR_Emulator(EmulatorBase):
     """Emulator that trains a GLM on data and a GPR on the residuals.
     """
+
     def __init__(
-            self,
-            glm_basis,
-            gpr_basis,
-            family = 'gaussian',
+        self, glm_basis, gpr_basis, family="gaussian",
     ):
         """Initialize the Emulator"""
-        if not isinstance(glm_basis,BasisBase):
+        if not isinstance(glm_basis, BasisBase):
             raise HistoryMatchingError("`glm_basis` must inherit from BasisBase!")
-        if not isinstance(gpr_basis,BasisBase):
+        if not isinstance(gpr_basis, BasisBase):
             raise HistoryMatchingError("`gpr_basis` must inherit from BasisBase!")
 
         self.glm_basis = glm_basis
@@ -132,7 +130,7 @@ class GLM_GPR_Emulator(EmulatorBase):
         self.glm = hm2.glm.GLM(family=family)
         self.gpr = hm2.gpr.SkGPR()
 
-    def _fit(self, train_x, train_y, stdev_y, glm_maxiter=1000, gpr_maxiter=1000):
+    def fit(self, train_x, train_y, stdev_y, glm_maxiter=1000, gpr_maxiter=1000):
         """Fit the GPR.
 
         Args:
@@ -144,13 +142,15 @@ class GLM_GPR_Emulator(EmulatorBase):
 
         Returns: None
         """
-        #Extract the relevant parameter sample values
+        train_x, train_y, stdev_y = self._fit(train_x, train_y, stdev_y)
+
+        # Extract the relevant parameter sample values
         train_x_glm = self.glm_basis(train_x)
         train_x_gpr = self.gpr_basis(train_x)
 
         self.glm.fit(train_x_glm, train_y, maxiter=glm_maxiter)
 
-        residuals = train_y-self.glm.predict(train_x_glm)
+        residuals = train_y - self.glm.predict(train_x_glm)
 
         self.gpr.fit(train_x_gpr, residuals, stdev_y, maxiter=gpr_maxiter)
 
@@ -166,6 +166,8 @@ class GLM_GPR_Emulator(EmulatorBase):
         Returns:
             Predicted outputs at the inputs specified by data.
         """
+        test_x = self._predict(test_x)
+
         test_x_glm = self.glm_basis(test_x)
         test_x_gpr = self.gpr_basis(test_x)
 
@@ -173,7 +175,5 @@ class GLM_GPR_Emulator(EmulatorBase):
 
     def plot_data(self, *args, **kwargs):
         return plot_data_multi(
-            self.glm_basis(self._train_x),
-            self._train_y,
-            *args, **kwargs
+            self.glm_basis(self._train_x), self._train_y, *args, **kwargs
         )
