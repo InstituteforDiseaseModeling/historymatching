35a9f1cbfb0e269a06d3b3d1ee9bcfa6417c6df7
Richard Barnes <rbarnes@umn.edu>
2020-04-10 22:06:37 -0600

    Added latin_hypercube_within()

--- hm2/hm2/sampling.py
+++ hm2/hm2/sampling.py
@@ -1,24 +1,17 @@
 from pyDOE import lhs
 import pandas as pd
 
-from .data_validation import ValidateParameterInfoFrame
+from .data_validation import *
 
 def latin_hypercube(param_info, samples):
   """
   Generate parameter hypercube given min and max values for parameters.
 
   Args:
-    param_info - A ParameterInfoFrame, e.g.:
+    param_info (ParameterInfoFrame): Bounds of the parameters.
+    samples (int): Number of samples to generate
 
-                      param_info = pd.DataFrame({
-                      'name': ['Beta', 'Gamma'],
-                      'min':  [  1e-6,    1e-6],
-                      'max':  [  0.01,     0.5]
-                      })
-
-    samples - Number of samples to generate for each parameter
-
-  Returns: A ParameterSamplesFrame
+  Returns: A :ref:`ParameterSamplesFrame`.
   """
   # Calculate ranges
   param_info = ValidateParameterInfoFrame(param_info)
@@ -38,3 +31,36 @@ def latin_hypercube(param_info, samples):
   samples['param_id'] = list(range(len(samples)))
 
   return samples
+
+
+
+def latin_hypercube_within(parameter_samples, samples):
+  """
+  Generate parameter hypercube bounded by another ParameterSamplesFrame.
+
+  Args:
+    parameter_samples (ParameterSamplesFrame): Parameter samples which bound
+        the new frame.
+    samples (int): Number of samples to generate for each parameter
+
+  Returns: A :ref:`ParameterSamplesFrame`.
+  """
+  # Strip down to parameters
+  parameter_samples = ValidateParameterSamplesFrame(parameter_samples)
+  parameter_samples = parameter_samples.drop(columns='param_id')
+
+  # Build a ParameterInfoFrame
+  params_and_ranges=[]
+  for x in parameter_samples:
+    params_and_ranges.append({
+      "name":x, 
+      "min":parameter_samples[x].min(), 
+      "max":parameter_samples[x].max()
+    })
+
+  params_and_ranges = pd.DataFrame(params_and_ranges)
+
+  print(params_and_ranges)
+
+  # Use parameter information to generate a new hypercube
+  return latin_hypercube(params_and_ranges, samples)
