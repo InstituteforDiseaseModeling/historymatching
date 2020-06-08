9b21316604c9555860def5dd97621da25f99f2c7
Richard Barnes <rbarnes@umn.edu>
2020-06-08 01:17:37 -0600

    Cleaning examples

--- examples/stochastic_sir_example.py
+++ examples/stochastic_sir_example.py
@@ -11,6 +11,10 @@ from hm2.plotting import *
 import hm2.basis
 import hm2.sampling
 
+# Set this to True to show plots. Plots are disabled by default to ensure this
+# script can be run by our automated testing framework.
+SHOW_PLOTS = False
+
 
 
 ################################################
@@ -50,10 +54,11 @@ param_info = pd.DataFrame({
 # Sampling to make the samples more uniformly spaced.
 parameter_samples = hm2.sampling.latin_hypercube(param_info, samples=50, random_state=123456)
 
-#TODO
-#p=plot_pairwise(parameter_samples.drop(columns='param_id'))
-#print(p['all'])
-
+# Save the plot in p so we can easily plot it again and again
+p=plot_pairwise(parameter_samples)
+if SHOW_PLOTS:
+  # `plot_pairwise` returns many plots. We view all of them at once this way.
+  print(p['all'])
 
 ################################################
 #Start sampling
@@ -64,8 +69,8 @@ parameter_samples = hm2.sampling.latin_hypercube(param_info, samples=50, random_
 
 def wrapped_model(**kwargs):
     model = SIR(**kwargs)
-
     results = model.run()
+    #Rename model result so it matches the name of an Observation
     results['prevalence'] = results['per_infected']
     #Reshape DataFrame into the tidy form expected by HistoryMatching
     results = pd.melt(results, id_vars='time', var_name='observation')
@@ -76,27 +81,26 @@ def wrapped_model(**kwargs):
     #Sort by time
     results.sort_values(by='time', inplace=True)
     return results, None
-    # return ValidateObservationFrames((results, None), copy=False)
+
 
 
 ########################################
 #WAVE 1
 ########################################
 
-################################################
 #Run the model a number of times
 sim_replicates = run_replicates(
   wrapped_model = wrapped_model,
-  param_sets    = [x.to_dict() for _, x in parameter_samples.iterrows()],
-  replicates    = 2,
-  processes     = None,
+  param_sets    = parameter_samples,
+  replicates    = 4,
+  processes     = None  # None implies that all cores are used to make multiple runs in parallel
 )
 
-#TODO
 #Just for fun, let's visualize some SIR trajectories and observations / target data
-#p = plot_runs_time_series(sim_replicates, samples=20, time_observations=time_observations)
-#Print plot every time you want to show it
-#print(p)
+p = plot_runs_time_series(sim_replicates, samples=20, time_observations=time_observations)
+if SHOW_PLOTS:
+  print(p)
+
 
 
 ################################################
@@ -107,31 +111,35 @@ matched = match_sim_outputs_to_observations(
   sim_replicates,
   time_observations,
   summary_observations,
-  processes=12
+  processes=None
 )
+
+
 
 ################################################
 #Fit emulators
 ################################################
 
-# Now we begin the process of emulating the simulation output
-# This process contains two steps.  The first step is to fit a deterministic model, here
-# we use a generalized linear model (GLM).  The glm will attempt to model the output (prevalence
-# at the first observation) as a function of some inputs.  Those inputs need not be the model
-# parameters directly!  The inputs could be anything from a constant intercept up to third or higher
-# order interaction terms between parameters.  The following Basis instance builds out the GLM input
-# parameters from the overall simulation input parameters.
+# Now we begin the process of emulating the simulation output This process
+# contains two steps.  The first step is to fit a deterministic model, here we
+# use a generalized linear model (GLM).  The glm will attempt to model the
+# output (prevalence at the first observation) as a function of some inputs.
+# Those inputs need not be the model parameters directly!  The inputs could be
+# anything from a constant intercept up to third or higher order interaction
+# terms between parameters.  The following Basis instance builds out the GLM
+# input parameters from the overall simulation input parameters.
 #
-# Some strategy is required when choosing these.  If you know which parameters matter, there's a way
-# to directly specify those parameters.  Alternatively, if you have no idea, you can initially include
-# an intercept, first, second, and maybe also third order interaction terms.  The Basis class has a
-# built-in penalized regression that throws away unneeded terms (basis vectors).
-# The second step of emulation, as demonstrated here, fits a GPR to the redisual error between the
-# simulated outputs and the GLM estimates.  If the GLM fits really well, the residual is mostly noise and
-# the GPR has a hard time fitting / isn't very informative.  I actually prefer to weaken the GLM enough
-# to leave plenty of residual signal for the GPR.  Here, I use only first-order (beta and gamma) terms.
-
-psamples_within = hm2.sampling.latin_hypercube_within(parameter_samples, 1000)
+# Some strategy is required when choosing these.  If you know which parameters
+# matter, there's a way to directly specify those parameters.  Alternatively, if
+# you have no idea, you can initially include an intercept, first, second, and
+# maybe also third order interaction terms.  The Basis class has a built-in
+# penalized regression that throws away unneeded terms (basis vectors). The
+# second step of emulation, as demonstrated here, fits a GPR to the redisual
+# error between the simulated outputs and the GLM estimates.  If the GLM fits
+# really well, the residual is mostly noise and the GPR has a hard time fitting
+# / isn't very informative.  I actually prefer to weaken the GLM enough to leave
+# plenty of residual signal for the GPR.  Here, I use only first-order (beta and
+# gamma) terms.
 
 #Prepare data for emulator (make a train_x, train_y, stdev_y tuple)
 time_emulators = dict()
@@ -152,23 +160,33 @@ for obs, params, y, stdev in get_data_for_emulators(parameter_samples, matched[0
 
 #TODO: Validate emulators here
 
-
+# Propose new parameters to look at within the current sample space
+psamples_within  = hm2.sampling.latin_hypercube_within(parameter_samples, 1000)
+# Use the emulators to determine how plausible each of the proposed parameter
+# samples are
 implausibilities = get_implausibility(time_emulators, psamples_within, time_observations)
+# Each parameter sample has implausibility values associated with several
+# emulators. We want to find the maximum implausibility for each sample.
 implausibilities = max_implausibility_per_param(implausibilities)
-
 # The implausibility threshold determines how willing we are to retain regions
 # of parameter space that are inconsistent with the underlying data. A higher
 # threshold is more risk averse in that potentially good regions are less likely
-# to be rejected, however it will take more iterations/simulations to achieve results.
+# to be rejected; however, it will take more iterations/simulations to achieve
+# results. Let's filter so we're left with only non-implausible parameters.
 implausibilities = filter_implausibilities(implausibilities, threshold=0.2)
+# Finally, we extract the non-implausible parameters back into a
+# ParameterSamplesFrame
 plausible_params = get_plausible_parameters(implausibilities, psamples_within)
-
+# If there are no non-implausible parameters, we should stop
 if len(plausible_params)==0:
   print("No non-implausible parameter samples!")
 
 
+
 ################################
 # WAVE 2
+
+# Now, we repeat the above analysis in the reduced parameter space.
 
 parameter_samples = hm2.sampling.latin_hypercube_within(plausible_params, 100, random_state=654321)
 
