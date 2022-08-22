1858b6fdfc54624fd4bcdce312ed44f91c54694b
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2022-08-22 12:54:44 -0700

    Test and example clean up.

--- history_matching/examples/Stochastic SIR/test.py
+++ history_matching/examples/Stochastic SIR/test.py
@@ -1,5 +1,12 @@
-from sir import SIR
+#! /usr/bin/env python3
+
+from pathlib import Path
+
+WORK_DIR = Path(__file__).parent.absolute()
+
+from history_matching.examples.sir import SIR
 
 z = SIR()
 z.sim()
-z.plot()
+fig, _ = z.plot()
+fig.savefig(WORK_DIR / "Stochastic_SIR.png")
