5319c51fa83ce5ae8fcee56a69f1d04c909c8032
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2023-02-18 00:09:30 +0000

    :construction: cut/next point generation

--- hm2/__init__.py
+++
@@ -1,8 +0,0 @@
-# stage/step/iteration/phase
-
-from .config import Config
-from .situation import Situation
-from .recipe import Recipe
-from .step import do_step, do_staircase
-
-__version__ = "0.1.0"
